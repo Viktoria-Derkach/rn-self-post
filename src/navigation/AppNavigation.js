f25976f000d78be38de66c5865d866122428661e
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2023-08-19 14:19:10 +0300

    bottom navigation

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,8 +1,13 @@
+import React from 'react';
 import { createAppContainer, ThemeColors } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+// import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
+import { BookedScreen } from '../screens/BookedScreen';
 import { THEME } from '../theme';
 
 const PostNavigator = createStackNavigator(
@@ -23,4 +28,44 @@ const PostNavigator = createStackNavigator(
   }
 );
 
-export const AppNavigation = createAppContainer(PostNavigator);
+const BookedNavigator = createStackNavigator(
+  {
+    Booked: BookedScreen,
+    Post: PostScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
+      },
+      headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
+    },
+  }
+);
+
+const bottomTabsConfig = {
+  Post: {
+    screen: PostNavigator,
+    navigationOptions: {
+      tabBarLabel: 'Все',
+      tabBarIcon: info => <Ionicons name="ios-albums" size={25} color={info.tintColor} />,
+    },
+  },
+  Booked: {
+    screen: BookedNavigator,
+    navigationOptions: {
+      tabBarLabel: 'Избранное',
+      tabBarIcon: info => <Ionicons name="ios-star" size={25} color={info.tintColor} />,
+    },
+  },
+};
+
+const BottomNavigator = createMaterialBottomTabNavigator(bottomTabsConfig, {
+  activeTintColor: '#fff',
+  shifting: true,
+  barStyle: {
+    backgroundColor: THEME.MAIN_COLOR,
+  },
+});
+
+export const AppNavigation = createAppContainer(BottomNavigator);

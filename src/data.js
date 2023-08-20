ede499a4e37c349f01f88357d2f1bbedd6efe1c5
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2023-08-20 22:35:21 +0300

    delete post

--- src/data.js
+++ src/data.js
@@ -1,42 +1,37 @@
 export const DATA = [
   {
     id: '1',
-    img:
-      'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg',
+    img: 'https://api.time.com/wp-content/uploads/2023/03/Worlds-Greatest-Places-Barcelona-Spain.jpg',
     text: 'Awesome text for post 1',
     date: new Date().toJSON(),
-    booked: true
+    booked: true,
   },
   {
     id: '2',
-    img:
-      'https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg',
+    img: 'https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg',
     text: 'Awesome text for post 2',
     date: new Date().toJSON(),
-    booked: true
+    booked: true,
   },
   {
     id: '3',
-    img:
-      'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg',
+    img: 'https://www.thoughtco.com/thmb/C1Nbj1M6jZK9Xm_lgD1ndDPPAk4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/singapore--garden-by-the-bay--supertree-grove-638256268-fa59e7e78b6449aaa40f68eafe6ff1a1.jpg',
     text: 'Awesome text for post 3',
     date: new Date().toJSON(),
-    booked: false
+    booked: false,
   },
   {
     id: '4',
-    img:
-      'https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg',
+    img: 'https://static.independent.co.uk/2023/07/12/14/iStock-1124556360.jpg',
     text: 'Awesome text for post 4',
     date: new Date().toJSON(),
-    booked: false
+    booked: false,
   },
   {
     id: '5',
-    img:
-      'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg',
+    img: 'https://planetofhotels.com/guide/sites/default/files/styles/big_gallery_image/public/text_gallery/milan-7.jpg',
     text: 'Awesome text for post 5',
     date: new Date().toJSON(),
-    booked: false
-  }
-]
+    booked: false,
+  },
+];
